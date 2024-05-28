c3faa4be9491caaabd544c18dac5c2c55a0bb702
Hatem Soliman <hatemthedev@gmail.com>
2024-05-28 13:23:15 +0300

    Exercise 2 - Sample 1

--- sample1.pl
+++ sample1.pl
@@ -14,3 +14,30 @@ path_connects(X, Y, [X, Y]):-
 path_connects(X, Y, [X|Path]):-
   edge(X, N),
   path_connects(N, Y, Path).
+
+
+% Exercise 2
+% a)
+member(A, [A|_]).
+member(A, [_|As]):-
+  member(A, As).
+
+difference(List, Diff):-
+  member(E1, List),
+  member(E2, List),
+  E1 > E2,
+  Diff is E1 - E2.
+
+
+% b)
+all_diffs(NumList, DiffList):-
+  bagof(D, difference(NumList, D), DiffList).
+
+
+% c)
+all_diffs_differ(NumList):-
+  % bagof(D, difference(NumList,D), DiffBag),
+  all_diffs(NumList, DiffBag), 
+  setof(D, difference(NumList, D), DiffSet),
+  length(DiffBag, L),
+  length(DiffSet, L).
