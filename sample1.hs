3b7db56be2c84de2c1f8eb2da3231e1c08f26722
Hatem Soliman <hatemthedev@gmail.com>
2024-05-28 15:22:17 +0300

    Exercise 4 - Sample 1

--- sample1.hs
+++ sample1.hs
@@ -10,3 +10,24 @@ duplicate str = concat [[c | _ <- [1..i]] | (c, i) <- zip str [1..]]
 
 
 -- Exercise 4
+-- a)
+andRec :: [Bool] -> Bool
+andRec [] = True
+andRec (x:xs) = x && andRec xs
+
+andHigher :: [Bool] -> Bool
+andHigher xs = foldr (&&) True xs
+
+
+-- b)
+unequalsRec :: [(Int, Int)] -> [(Int, Int)]
+unequalsRec list = [(x, y)|(x, y) <- list, x /= y]
+
+unequalsHigher :: [(Int, Int)] -> [(Int, Int)]
+unequalsHigher ((x, y):xs) = filter (\(x, y) -> x /= y) ((x, y):xs)
+
+
+-- c)
+filterAll :: [a -> Bool] -> [a] -> [a]
+filterAll [] list = list
+filterAll (f:fs) (x:xs) = filterAll fs (filter f (x:xs))
