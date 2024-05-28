1b2c4b5c13a63dbbb62cee5b6f2ee85682d3e26a
Hatem Soliman <hatemthedev@gmail.com>
2024-05-28 18:03:03 +0300

    Exercise 5 - Sample 1

--- sample1.hs
+++ sample1.hs
@@ -31,3 +31,25 @@ unequalsHigher ((x, y):xs) = filter (\(x, y) -> x /= y) ((x, y):xs)
 filterAll :: [a -> Bool] -> [a] -> [a]
 filterAll [] list = list
 filterAll (f:fs) (x:xs) = filterAll fs (filter f (x:xs))
+
+
+-- Exercise 5
+-- a)
+data Tree a = Leaf a | Node a (Tree a) (Tree a)
+zipTree :: Tree a -> Tree b -> Tree (a, b)
+zipTree (Leaf x) (Leaf y) = Leaf (x, y)
+zipTree (Leaf x) (Node y _ _) = Leaf (x, y)
+zipTree (Node x _ _) (Leaf y) = Leaf (x, y)
+zipTree (Node x left1 right1) (Node y left2 right2) = Node (x, y) (zipTree left1 left2) (zipTree right1 right2)
+
+
+-- b)
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f (Leaf x) = Leaf (f x)
+mapTree f (Node x left1 right1) = Node (f x) (mapTree f left1) (mapTree f right1)
+
+
+-- c)
+foldrTree :: (a -> b -> b -> b) -> (a -> b) -> Tree a -> b
+foldrTree f g (Leaf x) = g x
+foldrTree f g (Node x right left) = f x (foldrTree f g right) (foldrTree f g left)
